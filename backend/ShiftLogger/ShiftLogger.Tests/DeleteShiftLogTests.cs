a9247bb9cf108f1e7f0dad61beba5d3cebf2bfd9
Alex Almeida <alex.almeida@carguero.com.br>
2021-05-30 17:37:07 -0300

    add one more test for deleting

--- backend/ShiftLogger/ShiftLogger.Tests/DeleteShiftLogTests.cs
+++ backend/ShiftLogger/ShiftLogger.Tests/DeleteShiftLogTests.cs
@@ -23,5 +23,12 @@ namespace ShiftLogger.Tests
             resp.EnsureSuccessStatusCode();
             resp.StatusCode.Should().Be(HttpStatusCode.NoContent);
         }
+        
+        [Fact]
+        public async Task Should_return_bad_request_when_it_not_possible_remove_shift()
+        {
+            var resp = await _httpClient.DeleteAsync("api/shift/9999999");
+            resp.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        }
     }
 }
